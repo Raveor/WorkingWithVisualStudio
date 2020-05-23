30aec9a99ad7c598eadca55e65d2e59a92edd887
Maciej Książkiewicz <ksiazkiewiczmaciej@outlook.com>
2020-05-23 15:04:59 +0200

    data class

--- WorkingWithVisualStudio.Tests/HomeControllerTests.cs
+++ WorkingWithVisualStudio.Tests/HomeControllerTests.cs
@@ -10,13 +10,7 @@ namespace WorkingWithVisualStudio.Tests
     {
         class ModelCompleteFakeRepository : IRepository
         {
-            public IEnumerable<Product> Products { get; } = new Product[]
-            {
-                new Product {Name = "P1", Price = 275M},
-                new Product {Name = "P2", Price = 48.95M},
-                new Product {Name = "P3", Price = 19.50M},
-                new Product {Name = "P4", Price = 34.95M}
-            };
+            public IEnumerable<Product> Products { get; set; }
 
             public void AddProduct(Product p)
             {
@@ -24,39 +18,15 @@ namespace WorkingWithVisualStudio.Tests
             }
         }
 
-        class ModelCompleteFakeRepositoryPricesUnder50 : IRepository
+        [Theory]
+        [ClassData(typeof(ProductTestData))]
+        public void IndexActionModelIsComplete(Product[] products)
         {
-            public IEnumerable<Product> Products { get; } = new Product[]
+            var controller = new HomeController();
+            controller.Repository = new ModelCompleteFakeRepository()
             {
-                new Product {Name = "P1", Price = 5M},
-                new Product {Name = "P2", Price = 48.95M},
-                new Product {Name = "P3", Price = 19.50M},
-                new Product {Name = "P4", Price = 34.95M}
+                Products = products
             };
-
-            public void AddProduct(Product p)
-            {
-
-            }
-        }
-
-        [Fact]
-        public void IndexActionModelIsCompletePricesUnder50()
-        {
-            var controller = new HomeController();
-            controller.Repository = new ModelCompleteFakeRepositoryPricesUnder50();
-
-            var model = (controller.Index() as ViewResult)?.ViewData.Model as IEnumerable<Product>;
-
-            Assert.Equal(controller.Repository.Products, model, Comparer.Get<Product>((p1, p2) => p1.Name == p2.Name && p1.Price == p2.Price));
-        }
-
-
-        [Fact]
-        public void IndexActionModelIsComplete()
-        {
-            var controller = new HomeController();
-            controller.Repository = new ModelCompleteFakeRepository();
 
             var model = (controller.Index() as ViewResult)?.ViewData.Model as IEnumerable<Product>;
 
