7e8acd6723de34405327cdc8ac3a503d1d4e5f39
Maciej Książkiewicz <ksiazkiewiczmaciej@outlook.com>
2020-05-23 15:48:40 +0200

    moq

--- WorkingWithVisualStudio.Tests/HomeControllerTests.cs
+++ WorkingWithVisualStudio.Tests/HomeControllerTests.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
 using System.Collections.Generic;
 using WorkingWithVisualStudio.Controllers;
 using WorkingWithVisualStudio.Models;
@@ -8,59 +9,30 @@ namespace WorkingWithVisualStudio.Tests
 {
     public class HomeControllerTests
     {
-        class ModelCompleteFakeRepository : IRepository
-        {
-            public IEnumerable<Product> Products { get; set; }
-
-            public void AddProduct(Product p)
-            {
-
-            }
-        }
-
         [Theory]
         [ClassData(typeof(ProductTestData))]
         public void IndexActionModelIsComplete(Product[] products)
         {
-            var controller = new HomeController();
-            controller.Repository = new ModelCompleteFakeRepository()
-            {
-                Products = products
-            };
 
+            var mock = new Mock<IRepository>();
+            mock.SetupGet(m => m.Products).Returns(products);
+            var controller = new HomeController { Repository = mock.Object };
             var model = (controller.Index() as ViewResult)?.ViewData.Model as IEnumerable<Product>;
 
             Assert.Equal(controller.Repository.Products, model, Comparer.Get<Product>((p1, p2) => p1.Name == p2.Name && p1.Price == p2.Price));
         }
 
-        class PropertyOnceFakeRepository : IRepository
-        {
-            public int PropertyCounter { get; set; } = 0;
-
-            public IEnumerable<Product> Products
-            {
-                get
-                {
-                    PropertyCounter++;
-                    return new[] { new Product { Name = "P1", Price = 100 } };
-                }
-            }
-
-            public void AddProduct(Product p)
-            {
-
-            }
-        }
 
         [Fact]
         public void RepositoryPropertyCalledOnce()
         {
-            var repo = new PropertyOnceFakeRepository();
-            var controller = new HomeController { Repository = repo };
+            var mock = new Mock<IRepository>();
+            mock.SetupGet(m => m.Products).Returns(new[] { new Product { Name = "P1", Price = 100 } });
+            var controller = new HomeController { Repository = mock.Object};
 
             var result = controller.Index();
 
-            Assert.Equal(1, repo.PropertyCounter);
+            mock.VerifyGet(m => m.Products, Times.Once);
         }
     }
 }
