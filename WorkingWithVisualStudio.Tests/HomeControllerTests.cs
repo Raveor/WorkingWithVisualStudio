8809389f839a9e8536e727f65f500f5497092c89
Maciej Książkiewicz <ksiazkiewiczmaciej@outlook.com>
2020-05-23 15:44:39 +0200

    property counter

--- WorkingWithVisualStudio.Tests/HomeControllerTests.cs
+++ WorkingWithVisualStudio.Tests/HomeControllerTests.cs
@@ -32,5 +32,35 @@ namespace WorkingWithVisualStudio.Tests
 
             Assert.Equal(controller.Repository.Products, model, Comparer.Get<Product>((p1, p2) => p1.Name == p2.Name && p1.Price == p2.Price));
         }
+
+        class PropertyOnceFakeRepository : IRepository
+        {
+            public int PropertyCounter { get; set; } = 0;
+
+            public IEnumerable<Product> Products
+            {
+                get
+                {
+                    PropertyCounter++;
+                    return new[] { new Product { Name = "P1", Price = 100 } };
+                }
+            }
+
+            public void AddProduct(Product p)
+            {
+
+            }
+        }
+
+        [Fact]
+        public void RepositoryPropertyCalledOnce()
+        {
+            var repo = new PropertyOnceFakeRepository();
+            var controller = new HomeController { Repository = repo };
+
+            var result = controller.Index();
+
+            Assert.Equal(1, repo.PropertyCounter);
+        }
     }
 }
